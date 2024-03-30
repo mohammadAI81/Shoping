c1abcc12a3b9245728dab7fe04c1b2dea153d747
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-03-30 19:30:41 +0330

    make static folder & set setting for static file & add [css, js, vendors] to static folder

--- config/settings.py
+++ config/settings.py
@@ -122,14 +122,15 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
