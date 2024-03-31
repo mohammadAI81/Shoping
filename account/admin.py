4355bc93b6c29a39758bcc54cbf4713e8281480f
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-03-31 21:57:52 +0330

    set in admin of store app show model for admin panel

--- account/admin.py
+++ account/admin.py
@@ -6,8 +6,8 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomAdminPanel(UserAdmin):
-    list_display_links = ('id', 'username')
-    list_display = ('id', 'username', 'gender', 'is_staff')
+    list_display_links = ('username', )
+    list_display = ('username', 'gender', 'is_staff')
     list_editable = ('gender', )
     ordering = ('username', 'date_joined')
     list_filter = ('is_staff', 'is_superuser')
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Color, Category, Comment, Product, Like, Brand
+
+
+class CommentProduct(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    ordering = ('name', )
+    list_per_page = 10
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    list_per_page = 10
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_per_page = 10
+    ordering = ('name',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'rating', 'published')
+    list_editable = ('product', 'published')
+    list_filter = ('published', 'rating')
+    list_per_page = 25
+    ordering = ('datetime_created', )
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('person', 'product')
+    list_filter = ('person', )
+    list_per_page = 50
+    ordering = ('person', 'product')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'inventory', 'category', 'price', 'discount', 'availability')
+    list_editable = ('inventory', 'price', 'discount')
+    list_filter = ('category', 'discount', 'availability')
+    list_per_page = 35
+    ordering = ('name', 'category')
+    inlines = [
+        CommentProduct,
+    ]
+
