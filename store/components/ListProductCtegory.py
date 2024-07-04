c45e4cc625614c5b3ddd25b450d1d6cc66eadfc9
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-07-04 19:09:54 +0330

    Add categories and filters that include brand and color With django unicorn

---
+++ store/components/ListProductCtegory.py
@@ -0,0 +1,72 @@
+from django.core.paginator import Paginator
+from django.db.models import Q, Count
+from django_unicorn.components import UnicornView
+
+from ..models import Product, Color, Category, Brand
+from ..variable import SHOW, SORT
+
+
+class ListproductctegoryView(UnicornView):
+    color, brand = '', ''
+    sorts = SORT
+    shows = SHOW
+    categories = Category.objects.none()
+    brands = Brand.objects.none()
+    colors = Color.objects.none()
+    products = Product.objects.none()
+    values = {'sort': 'name', 'show': 'All'}
+
+    def mount(self):
+        # For Filter
+        # if sort := request_get.get('sort'):
+        #     products = products.order_by(sort)
+        #
+        # category = request_get.get('category')
+        # brand = request_get.get('brand')
+        # color = request_get.get('color')
+        # if category or brand or color:
+        #     q_objects = Q()
+        #     if category:
+        #         q_objects |= Q(category__name=category)
+        #     if brand:
+        #         q_objects &= Q(brand__name=brand)
+        #     if color:
+        #         q_objects &= Q(color__name=color)
+        #     products = products.filter(q_objects)
+        #
+        # context = {'products': products, 'num': request_get.get('page_num')}
+
+        # Pagination
+        # page_num = request_get.get('page')
+        # if context['num'] is None:
+        #     context['num'] = request_get.get('num')
+        # if page_num or context['num']:
+        #     if context['num'] != 'All':
+        #         paginator = Paginator(products, context['num'])
+        #         products = paginator.get_page(page_num)
+        #         context['page_obj'] = products
+        print('mount')
+        self.categories = Category.objects.all().values('id', 'name')
+        self.brands = Brand.objects.all().values('id', 'name')
+        self.colors = Color.objects.all().values('id', 'name')
+        self.products = Product.objects.select_related('category', 'color', 'brand')
+        # Filter Of Category
+        if self.request.GET:
+            category_id = self.request.GET.get('category')
+            if category_id != '0':
+                self.products = self.products.filter(category_id=category_id)
+        
+    def product_filter(self):
+        # Filter Of Color & Brand
+        if self.color or self.brand:
+            q_obj = Q()
+            if self.color:
+                q_obj &= Q(color_id=self.color)
+            if self.brand:
+                q_obj &= Q(brand_id=self.brand)
+            self.products = self.products.filter(q_obj)
+        self.sorts = SORT
+        self.shows = SHOW
+
+
+
--- store/views.py
+++ store/views.py
@@ -5,57 +5,28 @@ from django.views.generic import DetailView
 from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Category, Like, Color, Comment, Product, Brand
-from .variable import SHOW, SORT
+from .models import Comment, Product
 from .forms import CommentForm
 
 
 def list_product_category(request):
-    products = Product.objects.select_related('category', 'color', 'brand')
-    request_get = request.GET
+    # products = Product.objects.select_related('category', 'color', 'brand')
+    #
+    # context = {'products': products}
 
-    # For Filter
-    if sort := request_get.get('sort'):
-        products = products.order_by(sort)
+    # # Pagination
+    # page_num = request_get.get('page')
+    # if context['num'] is None:
+    #     context['num'] = request_get.get('num')
+    # if page_num or context['num']:
+    #     if context['num'] != 'All':
+    #         paginator = Paginator(products, context['num'])
+    #         products = paginator.get_page(page_num)
+    #         context['page_obj'] = products
 
-    category = request_get.get('category')
-    brand = request_get.get('brand')
-    color = request_get.get('color')
-    if category or brand or color:
-        q_objects = Q()
-        if category:
-            q_objects |= Q(category__name=category)
-        if brand:
-            q_objects &= Q(brand__name=brand)
-        if color:
-            q_objects &= Q(color__name=color)
-        products = products.filter(q_objects)
+    # context['products'] = products
 
-    context = {'products': products, 'num': request_get.get('page_num')}
-
-    # Pagination
-    page_num = request_get.get('page')
-    if context['num'] is None:
-        context['num'] = request_get.get('num')
-    if page_num or context['num']:
-        if context['num'] != 'All':
-            paginator = Paginator(products, context['num'])
-            products = paginator.get_page(page_num)
-            context['page_obj'] = products
-
-    context['products'] = products
-
-    # For Sort & Show Num Paginator
-    context['sorts'] = SORT
-    context['shows'] = SHOW
-
-    # Count Of Category & Brand & Color
-    context['categories'] = Category.objects.all().annotate(num_product=Count('product'))
-    context['brands'] = Brand.objects.all().annotate(num_product=Count('product'))
-    context['colors'] = Color.objects.all().annotate(num_product=Count('product'))
-    context['values'] = request_get
-
-    return render(request, 'store/products.html', context)
+    return render(request, 'store/products.html')
 
 
 class DetailProduct(LoginRequiredMixin, DetailView):
