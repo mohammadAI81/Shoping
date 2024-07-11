e0a5413af2852a59a48b3d42cc1e1577bc9b04e3
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-07-12 00:18:27 +0330

    add comment & blog model & setting admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Blog
+
+
+class CommentOfBlog(admin.TabularInline):
+    model = Comment
+    extra = 1
+    
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published')
+    list_editable = ('published', )
+    list_filter = ('published', )
+    ordering = ('-datetime_created', )
+    list_per_page = 10
+    fieldsets = (
+        (None, {'fields': ('author', 'title', 'description', 'published', 'slug')}),
+    )
+    inlines = [
+        CommentOfBlog,
+    ]
+    readonly_fields = ('slug', )
+    
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'blog')
+    list_per_page = 25
+    ordering = ('-datetime_created', )
+    fieldsets = (
+        (None, {'fields': ['blog', 'author', 'description']}),
+    )
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class SortByDateTimeManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by('-datetime_created')
+
+
+class PublishedBlogManager(models.Manager):
+    def blog_published(self):
+        return self.get_queryset().filter(published=True)
+
+
+class Blog(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField()
+    published = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True, editable=True)
+    datetime_modified = models.DateTimeField(auto_now=True, editable=True)
+
+    object = SortByDateTimeManager()
+    object_published = PublishedBlogManager()
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True, editable=True)
+    datetime_modified = models.DateTimeField(auto_now=True, editable=True)
+
+    object = SortByDateTimeManager()
+
+    def __str__(self):
+        return f'{self.author} comment for {self.blog.title}'
