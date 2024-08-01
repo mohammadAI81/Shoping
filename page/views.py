e113460182253203c670ecbf15b9c42c761e7b5f
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-08-01 09:34:34 +0330

    page: Optimization for query on home page

--- page/views.py
+++ page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, F
 
 
 from store.models import Product
@@ -7,16 +7,14 @@ from blog.models import Blog
 
 def home(request):
     # Import Data | Blog & Product
-    blogs = Blog.object_published.annotate(count_comments=Count('comments'))
-    products = Product.objects.select_related('category', 'brand', 'color').annotate(count_comments=Count('comments'))
+    blogs = Blog.object_published.annotate(count_comments=Count('comments'), midcomment=F('count_comments') / 2)
+    products = Product.objects.select_related('category').annotate(count_comments=Count('comments'), midcomment=F('count_comments') / 2)
     
     # Popular Blog
-    comments_count = max([blog['count_comments'] for blog in blogs.values('count_comments')]) // 2
-    blogs = blogs.filter(count_comments__gte=comments_count).order_by('-count_comments')[:3]
+    blogs = blogs.filter(count_comments__gte=F('midcomment')).order_by('-count_comments')[:3]
 
     # Popular Product
-    product_count_comment = max([product['count_comments'] for product in products.values('count_comments')]) // 2
-    products = products.filter(count_comments__gte=product_count_comment).order_by('-count_comments')[:7]
+    products = products.filter(count_comments__gte=F('midcomment')).order_by('-count_comments')[:7]
     
     
     context = {
