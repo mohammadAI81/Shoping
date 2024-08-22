8eb390de2299e8052ac764ba35a0101f72bdaa4a
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-08-22 18:25:01 +0330

    Update Data

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,5 @@
-from typing import Any
 from django.db.models import Count
 from django.contrib import admin
-from django.db.models.query import QuerySet
-from django.http import HttpRequest
 
 from .models import Order, OrderItem
 
